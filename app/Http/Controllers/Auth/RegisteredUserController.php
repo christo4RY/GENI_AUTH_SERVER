a3baf7301ef47cace90ba975afd7732f98db366c
christopher4RY <arkarlin486@gmail.com>
2023-10-02 18:56:19 +0630

    user sso connect

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use App\Models\UserDetail;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Auth/Register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(RegisterRequest $request)
+    {
+        try {
+            $user = User::create([
+                'fullname'=>$request->firstname." ".$request->lastname,
+                'email'=>$request->email,
+                'password'=>bcrypt($request->password)
+            ]);
+
+            $detail = UserDetail::create([
+                'user_id'=>$user->id,
+                'firstname'=>$request->firstname,
+                'lastname'=>$request->lastname,
+                'avatar'=>$request->avatar,
+                'phone'=>$request->phone,
+                'country'=>$request->country,
+                'city'=>$request->city,
+                'address'=>$request->address,
+            ]);
+
+            $token = $user->createToken('GENI')->accessToken;
+        } catch (\Throwable $th) {
+            $th->getMessage();
+        }
+
+
+        // event(new Registered($user));
+
+        Auth::login($user);
+
+        return response()->json([
+            'data'=>[
+                'id'=>$user->id,
+                'token'=>$token,
+                'message'=>'user '.$user->fullname.' was created',
+            ]
+        ]);
+        // return redirect(RouteServiceProvider::HOME);
+    }
+}
---
+++ database/migrations/2023_10_02_090745_create_user_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('nrc')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
